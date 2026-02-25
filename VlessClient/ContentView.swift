27c5b15272c3a3726a211bebaa41b24ab571f73b
oli <oli@qq.com>
2026-02-25 10:31:06 +0800

    Initial Commit

---
+++ VlessClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VlessClient
+//
+//  Created by huiliu on 2026/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VlessClient/VlessClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  VlessClientApp.swift
+//  VlessClient
+//
+//  Created by huiliu on 2026/2/25.
+//
+
+import SwiftUI
+
+@main
+struct VlessClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
